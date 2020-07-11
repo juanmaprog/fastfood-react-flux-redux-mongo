9eb9a362e33ed78fde108cfd48058bc69fa0d9ac
juan <juanma.prog@gmail.com>
2020-07-11 13:28:03 +0200

    Server api en rojo

--- src-api/index.js
+++ src-api/index.js
@@ -4,6 +4,10 @@ const app = require("./server");
 // require('./database');
 
 // Server is listening
-app.listen(app.get("port"), () => {
-  console.log("Server on port", app.get("port"));
+// app.listen(app.get("port"), () => {
+//   console.log("Server on port", app.get("port"));
+// });
+
+app.listen(3004, () => {
+  console.log("Server on port", 3004);
 });
