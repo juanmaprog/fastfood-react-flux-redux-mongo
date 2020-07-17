94be17e7dcaff52131a66886e76ee2f4a1969489
juan <juanma.prog@gmail.com>
2020-07-17 13:27:32 +0200

    Aded unit test HelperString.getNewGUID() in green

---
+++ src-api/test/helpers/helperString.test.ts
@@ -0,0 +1,19 @@
+const getNewGUID = require("../../helpers/helperString");
+
+test("get getNewGUID() to equal not empty", () => {
+  expect(getNewGUID()).not.toBe("");
+});
+
+test("get getNewGUID() to equal not null", () => {
+  expect(getNewGUID()).not.toBeNull();
+});
+
+test("get getNewGUID() to equal  defined", () => {
+  expect(getNewGUID()).toBeDefined();
+});
+
+test("get getNewGUID().length to equal length 13 ", () => {
+  const oid = getNewGUID();
+  const len = oid.length;
+  expect(len).toBe(36);
+});
