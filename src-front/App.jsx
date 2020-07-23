7aec264238cf8f3aac2d6ad9180ca324f4e57963
josebayarri <josemiguelbayarri@gmail.com>
2020-07-23 09:35:53 +0200

    login component created

--- src-front/App.jsx
+++ src-front/App.jsx
@@ -1,14 +1,15 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-import Header from "./components/header.jsx";
 import './components/header.css';
+import Login from "./components/login/login.jsx";
+
 
 
 function App() {
   return (
     <div className="App">
-        <Header></Header>
+        <Login></Login>
     </div>
   );
 }
