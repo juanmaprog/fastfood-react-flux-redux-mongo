93a12500374ebc7b762affe7f14680a582e734e8
josebayarri <josemiguelbayarri@gmail.com>
2020-07-23 11:06:46 +0200

    App.jsx modificated

--- src-front/App.jsx
+++ src-front/App.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import './components/header.css';
-import Login from "./components/login/login.jsx";
+import Login from "./components/Login/Login.jsx";
 
 
 
--- src-front/src/App.js
+++ src-front/src/App.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Login from './Login/Login';
+
 
 function App() {
   return (
@@ -10,6 +12,7 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
+        <Login></Login>
         <a
           className="App-link"
           href="https://reactjs.org"
