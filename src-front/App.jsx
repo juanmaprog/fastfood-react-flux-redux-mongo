21683c48804631268b8a830fca75a6d7222536cc
juanmaprog <juanma.prog@gmail.com>
2020-07-23 17:44:14 +0200

    Modified app.js

--- src-front/App.jsx
+++ src-front/App.jsx
@@ -1,5 +1,4 @@
 import React from 'react';
-<<<<<<< HEAD
 import logo from './logo.svg';
 import './App.css';
 import './components/header.css';
@@ -11,21 +10,6 @@ function App() {
   return (
     <div className="App">
         <Login></Login>
-=======
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
-import Register from "./components/register.jsx";
-
-function App() {
-  return (
-    <div>
-      <Router>
-
-        <Switch>
-          <Route path="/register" exact component={Register}></Route>
-        </Switch>
-        
-      </Router>
->>>>>>> 001bcc36a23daa74e459fb82522f69ced7c1a2d7
     </div>
   );
 }
