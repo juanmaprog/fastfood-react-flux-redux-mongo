f03ddeb556eff60c6ca1125f461c77908d5a0510
josebayarri <josemiguelbayarri@gmail.com>
2020-07-29 18:45:18 +0200

    home modificated

--- frontend/src/components/Home.js
+++ frontend/src/components/Home.js
@@ -6,23 +6,19 @@ export default class Home extends Component {
     render() {
         return (
           <div>
-<<<<<<< HEAD
             <h1 className="titulo_home">bienvenido a nuestro restaurante</h1>
             <div className="main">
+              
               <div className="left">
                 <h2>carta</h2>
-                <p></p>
+                <p>Hecha un vistazo a nuestras espectaculares hamburguesas y realiza tu pedido</p>
+
               </div>
               <div className="right">
                 <h2>ofertas</h2>
-                <p></p>
+                <p>Hecha un vistazo a nuestras espectaculares hamburguesas y realiza tu pedido</p>
               </div>
             </div>
-=======
-            <h1>LAS MEJORES HAMBURGUESAS PARA LOS MEJORES VEGETARIOS</h1>
-            <button>PIDE AHORA</button>
-            <BackgroundImagePage/>
->>>>>>> 5c1a633eb20789d1473b0fc924dd3d8cef5212b8
           </div>
         )
       }
